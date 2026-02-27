54494262d911674fa67814b7781bff2086c23476
anushagalla-dev <photosofanushagalla@gmail.com>
2026-02-27 08:01:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
 
+// Auto play music on first interaction (mobile safe)
+document.addEventListener("click", function () {
+  const music = document.getElementById("bgMusic");
+  if (music.paused) {
+    music.play().catch(() => {});
+  }
+}, { once: true });
